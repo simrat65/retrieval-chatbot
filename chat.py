049e659509b0e91839c0cd611fa418eb5c94115a
Simrat Singh <simrat65@gmail.com>
2018-06-18 12:11:10 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,13 @@
+
+"""
+Main script. See README.md for more information
+
+Use python 3
+"""
+
+from chatbot import chatbot_para as chatbot
+
+
+if __name__ == "__main__":
+    chatbot = chatbot.Chatbot()
+    chatbot.mainTest()
