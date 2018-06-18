4aecd42cb44f92bb53735f7d572c7896dad65b6f
Simrat Singh <simrat65@gmail.com>
2018-06-18 17:31:23 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,9 +5,9 @@ Main script. See README.md for more information
 Use python 3
 """
 
-from chatbot import chatbot_para as chatbot
+from main import chatbot as chatbot
 
 
 if __name__ == "__main__":
     chatbot = chatbot.Chatbot()
-    chatbot.mainTest()
+    chatbot.main()
